7c2f1b5f2d97fcd3d2ec66f698c0a725ccc44500
jglez <jglez.cs@gmail.com>
2021-02-19 01:20:08 -0600

    Added add item to cart functionality

--- src/App.js
+++ src/App.js
@@ -8,27 +8,36 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  const addItem = item => {
+    // add the given item to the cart
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+    // We have to use an array because <Navigation /> uses the length
+    // of the array to display how many items the user has in their cart
+    return setCart([
+      ...cart,
+      item
+    ])
+  };
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+  console.log(cart)
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Navigation cart={cart} />
+
+      {/* Routes */}
+      <Route exact path="/">
+        <Products products={products} addItem={addItem} />
+      </Route>
+
+      <Route path="/cart">
+        <ShoppingCart cart={cart} />
+      </Route>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Navigation = props => {
-	return (
-		<div className="navigation">
-			<NavLink to="/">Products</NavLink>
-			<NavLink to="/cart">
-				Cart <span>{props.cart.length}</span>
-			</NavLink>
-		</div>
-	);
+  return (
+    <div className="navigation">
+
+      <NavLink to="/">Products</NavLink>
+
+      <NavLink to="/cart">
+        Cart <span>{props.cart.length}</span>
+      </NavLink>
+
+    </div>
+  );
 };
 
 export default Navigation;
